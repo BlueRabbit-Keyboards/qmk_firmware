676172abd13b53027bc1192885129fe8f0bf31c5
Scott Tancock <st12660@my.bristol.ac.uk>
2020-07-29 18:26:38 +0100

    Initialise SPI.

--- keyboards/bluerabbit/wkb60/wkb60.c
+++ keyboards/bluerabbit/wkb60/wkb60.c
@@ -1,1 +1,10 @@
 #include "wkb60.h"
+#include "spi_master.h"
+
+void keyboard_pre_init_kb(void) { keyboard_pre_init_user(); }
+
+void keyboard_post_init_kb(void) {
+	spi_init(); 
+	spi_start(A4, FALSE, 0, 16);
+	keyboard_post_init_user();
+}
